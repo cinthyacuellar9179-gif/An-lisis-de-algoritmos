b7bc57f0dbb5227fc24d185b724f6f4d13c4ddc3
cinthyacuellar9179-gif <cinthya.cuellar9179@alumnos.udg.mx>
2025-11-29 19:26:44 -0600

    Implement quick sort algorithm in quickSort.py

--- [Practica ] Merge & QuickSort/mergeSort.py/quickSort.py
+++ [Practica ] Merge & QuickSort/mergeSort.py/quickSort.py
@@ -1,1 +1,17 @@
+def quick_sort(arr):       #Sofía Cuéllar
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
 
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left)+ middle + quick_sort(right)
+
+# ej
+
+arr = [7,38,27,43,3,9,82,10]
+sorted_arr = quick_sort(arr)
+print("Arreglo ordenado:", sorted_arr)
